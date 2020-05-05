d2aaaccff85a5ce9c8bf107ac61d44b5658a3327
Keshon12 <51712125+Keshon12@users.noreply.github.com>
2020-05-05 17:06:15 -0500

    Add files via upload

--- AsynchronousWebApps-master/Get Requets/script3.js
+++ AsynchronousWebApps-master/Get Requets/script3.js
@@ -3,7 +3,7 @@
 function getDogImage(breed) {
   fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
     .then(response => response.json())
-    .then(responseJson =>
+    .then(responseJson => 
       displayResults(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
 }
@@ -11,32 +11,33 @@ function getDogImage(breed) {
 function displayResults(responseJson) {
   console.log(responseJson);
 
-  if (responseJson.message == "Breed not found") {
+  if (responseJson.status == "error") {
     alert('That breed wasn\'t found, please try another.');
   } else {
-    //replace the existing image with the new one
-    $('.results').html(`<h2>Look at this dog!</h2>`);
+    // replace existing img with new one
+    $('.results').html(`<h2>Look at this pup!</h2>`);
 
     let splitUrl = responseJson.message.split("/");
     let breedName = splitUrl[4];
     $('.results').append(`<h3>${breedName}</h3>`);
 
     $('.results').append(
-      `<img src="${responseJson.message}" class="results-img" width="200" height="auto">`);
-    //display the results section
+      `<img src="${responseJson.message}" class="results-img" width="200" height="auto">`
+    );
+    // display results section
     $('.results').removeClass('hidden');
   }
 }
 
-function watchForm() {
+function watchUserInput() {
   $('form').submit(event => {
     event.preventDefault();
-    let breedOfDog = $('input[name="breedOfDog"]').val();
-    getDogImage(breedOfDog);
+    let dogBreed = $('input[name="dogBreed"]').val();
+    getDogImage(dogBreed);
   });
 }
 
-$(function () {
+$(function() {
   console.log('App loaded! Waiting for submit!');
-  watchForm();
+  watchUserInput();
 });
